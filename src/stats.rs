77fd508fc14e210697b1da7bc01e2f55fbaffdab
MCOfficer <mcofficer@gmx.de>
2020-10-16 11:03:11 +0200

    Implement dump creation

--- src/stats.rs
+++ src/stats.rs
@@ -3,8 +3,10 @@ use crate::{db, Opt};
 use anyhow::Result;
 use chrono::serde::ts_milliseconds;
 use chrono::{DateTime, Utc};
+use futures::StreamExt;
 use serde::{Deserialize, Serialize};
 use std::fs::File;
+use std::io::{BufWriter, Write};
 use wither::bson::doc;
 use wither::Model;
 
@@ -24,11 +26,43 @@ struct Dump {
     created: DateTime<Utc>,
 }
 
-pub async fn create_dump(opt: &Opt, _: &Database) -> Result<()> {
+pub async fn create_dump(opt: &Opt, db: &Database) -> Result<()> {
     info!("Creating dump");
 
+    let mut tempfile = opt.public_dir.clone();
+    tempfile.push("dump.zip.tmp");
+    let buffer = BufWriter::new(File::create(&tempfile)?);
+
+    let mut zipfile = zip::ZipWriter::new(buffer);
+    let inner_filename = format!("dump-{}.txt", Utc::now().format("%F-%H-%M-%S"));
+    zipfile.start_file(inner_filename, zip::write::FileOptions::default())?;
+
+    let ods: Vec<String> = db
+        .get_opendirectories(false)
+        .await?
+        .filter_map(|r| async { r.ok().filter(|l| l.unreachable < 5) })
+        .map(|od| od.url)
+        .collect()
+        .await;
+
+    let mut read = Box::pin(
+        db::Link::find(&db.db, None, None)
+            .await?
+            .filter_map(|r| async { r.ok().filter(|l| ods.contains(&l.opendirectory)) }),
+    );
+
+    while let Some(link) = read.next().await {
+        zipfile.write_all(format!("{}\n", link.url).as_bytes())?;
+    }
+
+    drop(zipfile);
+    let mut targetfile = opt.public_dir.clone();
+    targetfile.push("dump.zip");
+
+    std::fs::rename(tempfile, targetfile)?;
+
     let dump = Dump {
-        url: "STUB".to_string(),
+        url: "https://discovery.odcrawler.xyz/dump.zip".to_string(),
         links: 0,
         size: 0,
         created: Utc::now(),
